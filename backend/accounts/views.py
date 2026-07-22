777f76f0b01f6fd8f57b8eddba9d8349141a5012
situaciones <169110610+situaciones@users.noreply.github.com>
2026-07-21 20:46:11 -0400

    Corregir ImportError del serializador en accounts/views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,11 +2,11 @@ from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import DebidoTokenObtainPairSerializer, UsuarioSerializer
+from .serializers import VivePiolaTokenObtainPairSerializer, UsuarioSerializer
 
 
 class VivePiolaTokenObtainPairView(TokenObtainPairView):
-    serializer_class = DebidoTokenObtainPairSerializer
+    serializer_class = VivePiolaTokenObtainPairSerializer
 
 
 class MeView(RetrieveUpdateAPIView):
